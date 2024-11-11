8aef59e9d222ab2c212d0710c507d381d6fa12cd
JosephGonzalez1 <152119091+JosephGonzalez1@users.noreply.github.com>
2024-11-11 14:03:49 -0800

    L16 Dialog Startup Commit

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 // todo
+const button = document.createElement("button");
+button.textContent = "button";
+
+button.onclick = () => {
+  alert("You clicked the button!");
+};
+
+document.body.appendChild(button);
